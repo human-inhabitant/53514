06c977b5e34ff61c42de2170091d5249abc5da18
Human Inhabitant <human-inhabitant@pm.me>
2020-11-02 13:17:41 -0500

    Completed chapter 06...

--- meadowlark.js
+++ meadowlark.js
@@ -7,6 +7,7 @@ const handlers = require('./lib/handlers');
 const app = express();
 const port = process.env.PORT || 3e3;
 
+app.disable('x-powered-by');
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
@@ -15,6 +16,7 @@ app.use(express.static(`${__dirname}/public`));
 
 app.get('/', handlers.home);
 app.get('/about', handlers.about);
+app.get('/headers', handlers.headers);
 
 app.use(handlers.notFound);
 app.use(handlers.serverError);
